b4a019b5f4c47ebc96b99a723712ea05ea155e10
46160 <nevergiveup1999>
2022-12-01 10:55:21 +0800

    日志监控、零信任管理纯前端版本

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,50 +1,43 @@
 <template>
-  <div class="app-container" style="background-color: #efefef; height: 100%">
+  <div class="app-container" style="background-color: #f3f3f3; height: 100%">
     <el-row>
       <el-col span="3"><h2>日志监控面板</h2></el-col>
     </el-row>
-    <el-row style="text-align: right">
-      <el-button span="3" type="primary">日志下载</el-button>
+    <el-row>
+      <el-col :span=6>
+        日志级别筛选：
+        <el-select v-model="level" placeholder="请选择">
+          <el-option
+            v-for="item in levelOption"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"></el-option>
+        </el-select>
+      </el-col>
+      <el-col :span=6>
+        <span class="demonstration">日志日期筛选：</span>
+        <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="选择日期">
+      </el-date-picker>
+      </el-col>
+
+      <el-col span=12 style="text-align: right">
+        <el-button span="3" type="primary">日志下载</el-button>
+      </el-col>
     </el-row>
-    <el-row style="height: 780px">
-      <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto; height: 100%; background-color: white">
-        {{logText}}
-        .   ____          _            __ _ _<br />
-        /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \<br />
-        ( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \<br />
-        \\/  ___)| |_)| | | | | || (_| |  ) ) ) )<br />
-        '  |____| .__|_| |_|_| |_\__, | / / / /<br />
-        =========|_|==============|___/=/_/_/_/<br />
-        :: Spring Boot ::                (v2.7.5)<br />
-        <br />
-        2022-11-25 09:34:47.939  INFO 10840 --- [           main] com.cad.datalabel.DatalabelApplication   : Starting DatalabelApplication using Java 1.8.0_292 on Zheng-Y9000K with PID 10840 (D:\datalabel\target\classes started by 46160 in D:\datalabel)<br />
-        2022-11-25 09:34:47.941  INFO 10840 --- [           main] com.cad.datalabel.DatalabelApplication   : No active profile set, falling back to 1 default profile: "default"<br />
-        2022-11-25 09:34:48.899  INFO 10840 --- [           main] o.s.b.w.embedded.tomcat.TomcatWebServer  : Tomcat initialized with port(s): 8100 (http)<br />
-        2022-11-25 09:34:48.905  INFO 10840 --- [           main] o.apache.catalina.core.StandardService   : Starting service [Tomcat]<br />
-        2022-11-25 09:34:48.905  INFO 10840 --- [           main] org.apache.catalina.core.StandardEngine  : Starting Servlet engine: [Apache Tomcat/9.0.68]<br />
-        2022-11-25 09:34:48.974  INFO 10840 --- [           main] o.a.c.c.C.[Tomcat].[localhost].[/]       : Initializing Spring embedded WebApplicationContext<br />
-        2022-11-25 09:34:48.975  INFO 10840 --- [           main] w.s.c.ServletWebServerApplicationContext : Root WebApplicationContext: initialization completed in 997 ms<br />
-        Logging initialized using 'class org.apache.ibatis.logging.stdout.StdOutImpl' adapter.<br />
-        Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'. The driver is automatically registered via the SPI and manual loading of the driver class is generally unnecessary.<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\DatasetMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\DatasourceMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\LabelGroupMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\LabelMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\RowMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\TableCreateMapper.xml]'<br />
-        Parsed mapper file: 'file [D:\研究生\毕设\datalabel\target\classes\mapper\TextClassifiedRowMapper.xml]'<br />
-        <br />
-        <br />
-        ,------.                           ,--.  ,--.         ,--.<br />
-        |  .--. '  ,--,--.  ,---.   ,---.  |  '--'  |  ,---.  |  |  ,---   ,---.  ,--.--.<br />
-        |  '--' | ' ,-.  | | .-. | | .-. : |  .--.  | | .-. : |  | | .-. | | .-. : |  .--'<br />
-        |  | --'  \ '-'  | ' '-' ' \   --. |  |  |  | \   --. |  | | '-' ' \   --. |  |<br />
-        `--'       `--`--' .`-  /   `----' `--'  `--'  `----' `--' |  |-'   `----' `--'<br />
-        `---'                                   `--'                        is intercepting.<br />
-        <br />
-        2022-11-25 09:34:49.703  INFO 10840 --- [           main] o.s.b.w.embedded.tomcat.TomcatWebServer  : Tomcat started on port(s): 8100 (http) with context path ''<br />
-        2022-11-25 09:34:49.710  INFO 10840 --- [           main] com.cad.datalabel.DatalabelApplication   : Started DatalabelApplication in 2.061 seconds (JVM running for 2.78)<br />
-      </ul>
+    <el-row style="background-color: #f3f3f3">
+      <div style="height: 600px; margin-top: 20px; margin-right: 20px; margin-left: 20px; overflow-y: auto; position: absolute; width: 100%; border: black; border-bottom-width: 2px">
+        <div v-for="log in logItem" style="margin-left: 10px">
+          <div style="display: flex; text-align: left">
+            <p>{{log.date}}</p>
+            <p v-show="log.level==='info'" style="color: green; margin-left: 10px">{{log.level}}</p>
+            <p v-show="log.level==='warning'" style="color: red; margin-left: 10px">{{log.level}}</p>
+            <p style="margin-left: 10px">{{log.content}}</p>
+          </div>
+        </div>
+      </div>
     </el-row>
   </div>
 </template>
@@ -53,17 +46,94 @@
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-        logText: '   7:7   warning  Require self-closing on Vue.js custom components (<el-input>)           vue/html-self-closing\n'
-      }
+      levelOption: [{
+        value: 'all',
+        label: '全部'
+      }, {
+        value: 'info',
+        label: '一般日志'
+      }, {
+        value: 'warning',
+        label: '警告日志'
+      }],
+      logText: '   7:7   warning  Require self-closing on Vue.js custom components (<el-input>)           vue/html-self-closing\n',
+      level: '',
+      date: '',
+      logItem: [
+        {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'info',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }, {
+          'date': '2022-12-1',
+          'level': 'warning',
+          'content': '设备A请求服务A，信誉值计算：90，信誉阈值：80，放行'
+        }
+      ]
     }
   },
   methods: {
@@ -81,8 +151,5 @@ export default {
 </script>
 
 <style scoped>
-.line{
-  text-align: center;
-}
 </style>
 
--- src/views/tree/index.vue
+++ src/views/tree/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="app-container"  style="background-color: #efefef; height: 100%">
+  <div class="app-container"  style="background-color: #f3f3f3; height: 900px">
     <el-row>
-      <el-col span="3"><h2>零信任认证管理</h2></el-col>
+      <el-col :span=3><h2>零信任认证管理</h2></el-col>
     </el-row>
-    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
 
-    <div style="background-color: white">
+    <el-row>
+      <el-col span="4">
+        信誉值阈值：<el-input type="number" v-model="creditThreshold" style="width: 150px"></el-input>
+      </el-col>
+      <el-col span="5" offset=1>
+        请求次数阈值：<el-input type="number" v-model="reqThreshold" style="width: 150px"></el-input>
+      </el-col>
+      <el-col span=14 style="text-align: right">
+        <el-button type="primary" size="medium" >保存</el-button>
+      </el-col>
+    </el-row>
+    <el-divider></el-divider>
+
+    <div style="background-color: white; height: 900px">
       <el-row>
-        <el-col span="2">
+        <el-col :span=2>
           <el-tree
             ref="tree2"
             :data="data2"
@@ -15,10 +27,77 @@
             :filter-node-method="filterNode"
             class="filter-tree"
             default-expand-all
+            @node-click="handleNodeClick"
           />
         </el-col>
-        <el-col span="10" offset="2">
-          <el-form ref="form" :model="form" label-width="140px">
+        <el-col :span=10 :offset=2 v-show="hintShow">
+          <div>
+            <h2>请选择站点和设备</h2>
+          </div>
+        </el-col>
+        <el-col :span=20 :offset=2 v-show="!hintShow">
+
+          <el-table
+            v-loading="listLoading"
+            :data="list"
+            element-loading-text="Loading"
+            border
+            fit
+            highlight-current-row
+          >
+            <el-table-column align="center" label="ID" width="95">
+              <template slot-scope="scope">
+                {{ scope.$index }}
+              </template>
+            </el-table-column>
+            <el-table-column label="名称">
+              <template slot-scope="scope">
+                {{ scope.row.title }}
+              </template>
+            </el-table-column>
+            <el-table-column label="主体属性状态" width="200" align="center">
+              <template slot-scope="scope">
+                <el-tag type="success" v-show="scope.row.subject">{{ scope.row.subject }}</el-tag>
+                <el-tag type="danger" v-show="!scope.row.subject">{{ scope.row.subject }}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column label="环境属性状态" width="200" align="center">
+              <template slot-scope="scope">
+                <el-tag v-show="scope.row.environment" type="success">{{ scope.row.environment }}</el-tag>
+                <el-tag v-show="!scope.row.environment" type="danger">{{ scope.row.environment }}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column  label="客体属性状态" width="200" align="center">
+              <template slot-scope="scope">
+                <el-tag type="success" v-show="scope.row.object">{{ scope.row.object }}</el-tag>
+                <el-tag type="danger" v-show="!scope.row.object">{{ scope.row.object }}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column align="center" prop="created_at" label="创建时间" width="200">
+              <template slot-scope="scope">
+                <i class="el-icon-time" />
+                <span>{{ scope.row.display_time }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column align="center" prop="created_at" label="操作" width="200">
+              <template slot-scope="scope">
+
+                <el-button @click="drawer = true" type="primary" size="small" round>
+                  编辑属性
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-col>
+      </el-row>
+
+      <el-drawer
+        title="属性编辑"
+        :visible.sync="drawer"
+        :direction="direction"
+        :before-close="handleClose">
+        <div class="overflowAuto" style="margin-left: 20px; margin-bottom: 100px;">
+          <el-form ref="form" :model="form" label-width="150px">
             <h2>主体属性</h2>
             <el-form-item label="MAC地址">
               <el-input v-model="form.name" />
@@ -36,9 +115,8 @@
               <el-input v-model="form.name" />
             </el-form-item>
             <el-divider></el-divider>
-
             <h2>环境属性</h2>
-            <el-form-item label="IP">
+            <el-form-item label="IP网段">
               <el-input v-model="form.name" />
             </el-form-item>
             <el-form-item label="上线时间范围">
@@ -48,7 +126,6 @@
               <el-input v-model="form.name" />
             </el-form-item>
             <el-divider></el-divider>
-
             <h2>客体属性</h2>
             <el-form-item label="所属部门">
               <el-input v-model="form.name" />
@@ -62,9 +139,15 @@
             <el-form-item label="授权级别">
               <el-input v-model="form.name" />
             </el-form-item>
+            <el-form-item style="height: 100px">
+              <div style="text-align: center">
+                <el-button type="primary">保存</el-button>
+              </div>
+            </el-form-item>
+            <el-divider></el-divider>
           </el-form>
-        </el-col>
-      </el-row>
+        </div>
+      </el-drawer>
     </div>
 
   </div>
@@ -75,6 +158,12 @@ export default {
 
   data() {
     return {
+      drawer: false,
+      creditThreshold: '',
+      reqThreshold: 0,
+      direction: 'rtl',
+      selectDevice: '',
+      hintShow: true,
       filterText: '',
       form: {
         name: '',
@@ -89,38 +178,58 @@ export default {
       data2: [{
         id: 1,
         label: '站点1',
+        type: 1,
         children: [{
           id: 4,
+          type: 2,
           label: '设备1'
         }, {
           id: 5,
+          type: 2,
           label: '设备2'
         }]
       }, {
         id: 2,
         label: '站点2',
+        type: 1,
         children: [{
           id: 5,
+          type: 2,
           label: '设备1'
         }, {
           id: 6,
+          type: 2,
           label: '设备2'
         }]
       }, {
         id: 3,
         label: '站点3',
+        type: 1,
         children: [{
           id: 7,
+          type: 2,
           label: '设备1'
         }, {
           id: 8,
+          type: 2,
           label: '设备2'
         }]
       }],
       defaultProps: {
         children: 'children',
         label: 'label'
-      }
+      },
+      list: [
+        {
+          subject: true,
+          environment: true,
+          object: true
+        }, {
+          subject: false,
+          environment: false,
+          object: false
+        }
+      ]
     }
   },
   watch: {
@@ -142,8 +251,30 @@ export default {
         message: 'cancel!',
         type: 'warning'
       })
+    },
+
+    handleNodeClick(data) {
+      console.log(data)
+      if (data.type == 2) {
+        this.hintShow = false
+        this.selectDevice = data
+      }
     }
   }
 }
 </script>
+<style>
+/deep/ .el-drawer__body {
+  overflow: auto;
+}
+/deep/ .el-drawer__container ::-webkit-scrollbar{
+  display: none;
+}
+.overflowAuto {
+  overflow-y: auto;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
 
